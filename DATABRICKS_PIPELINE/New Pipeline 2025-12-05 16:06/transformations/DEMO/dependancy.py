16c32cbb85a75953f10b20b153f5d9e1255e60a8
aryansapsod <sapsodaryan@gmail.com>
2025-12-05 14:24:02 +0000

    Added CoreComponents Added New Notebook

--- DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/dependancy.py
+++ DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/dependancy.py
@@ -4,3 +4,29 @@ import dlt
 
 #  Staging Area
 
+@dlt.table(
+    name = "staging_orders"
+)
+def staging_orders():
+    # return(
+    #     spark.readStream.format('cloudFiles') 
+
+    # )
+    df = spark.readStream.table('dlt.source.orders')
+    return df
+
+
+
+#  Creating Transform Area
+
+
+@dlt.table(
+    name="transform_orders"
+)
+
+def transform_orders():
+    df =spark.readStream.table('staging_orders')
+    
+    return df
+
+    
