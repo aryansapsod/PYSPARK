e1d8b70fde045e1774243d2d3387b04cc1f2b3ae
aryansapsod <sapsodaryan@gmail.com>
2025-12-06 13:48:35 +0000

    Comment Out All Things And New Data

--- DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/Corecomponent.py
+++ DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/Corecomponent.py
@@ -1,42 +1,54 @@
-# Creating straming table
+# # Creating straming table
 
-import dlt
+# import dlt
 
-@dlt.table(
-  name = "streaming_table",
+# @dlt.table(
+#   name = "streaming_table",
 
-)
-def first_stream_table():
+# )
+# def first_stream_table():
    
-   df = spark.readStream.table("dlt.source.orders")
-   return df
+#    df = spark.readStream.table("dlt.source.orders")
+#    return df
  
-@dlt.table(
-  name = 'first_mat_view'
-)
+# @dlt.table(
+#   name = 'first_mat_view'
+# )
 
-def first_mat_view():
+# def first_mat_view():
   
-  df = dlt.readStream("streaming_table")
-  return df
+#   df = dlt.readStream("streaming_table")
+#   return df
 
-#  Create batch Normal View
+# #  Create batch Normal View
 
-@dlt.view(
-  name = 'batch_normal_view'
-)
-def batch_normal_view(): 
-  df = spark.read.table("dlt.source.orders")
-  return df
+# @dlt.view(
+#   name = 'batch_normal_view'
+# )
+# def batch_normal_view(): 
+#   df = spark.read.table("dlt.source.orders")
+#   return df
 
-# Create Streming View
+# # Create Streming View
 
-@dlt.view(
-  name = 'streaming_view'
-)
-def streaming_view(): 
-  df = spark.readStream.table("dlt.source.orders")
-  return df
+# @dlt.view(
+#   name = 'streaming_view'
+# )
+# def streaming_view(): 
+#   df = spark.readStream.table("dlt.source.orders")
+#   return df
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/dependancy.py
+++ DATABRICKS_PIPELINE/New Pipeline 2025-12-05 16:06/transformations/DEMO/dependancy.py
@@ -1,32 +1,47 @@
-import dlt
+# import dlt
+# from pyspark.sql.functions import *
 
-# Creating End To End Pipeline
+# # Creating End To End Pipeline
 
-#  Staging Area
+# #  Staging Area
 
-@dlt.table(
-    name = "staging_orders"
-)
-def staging_orders():
-    # return(
-    #     spark.readStream.format('cloudFiles') 
+# @dlt.table(
+#     name = "staging_orders"
+# )
+# def staging_orders():
+#     # return(
+#     #     spark.readStream.format('cloudFiles') 
 
-    # )
-    df = spark.readStream.table('dlt.source.orders')
-    return df
-
-
-
-#  Creating Transform Area
+#     # )
+#     df = spark.readStream.table('dlt.source.orders')
+#     # df = df.withColumn
+#     return df
 
 
-@dlt.table(
-    name="transform_orders"
-)
 
-def transform_orders():
-    df =spark.readStream.table('staging_orders')
+# #  Creating Transform Area
+
+
+# @dlt.table(
+#     name="transform_orders"
+# )
+
+# def transform_orders():
+#     df =spark.readStream.table('staging_orders')
+#     df = df.withColumn("orderstatus",lower(col("order_status")))
+
     
-    return df
+#     return df
 
-    
+
+# #  Creating Agrigating Area
+
+# @dlt.table(
+#     name="aggregating_orders"
+# )
+# def aggregating_orders():
+#     df = spark.readStream.table('transform_orders')
+#     df = df.groupBy("orderstatus").count()
+#     return df
+
+
